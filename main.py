1e88544b8cd15da2303dea8103b2c8e735a92c22
abe mcerlean <abemacminecraft@outlook.com>
2024-06-20 19:40:45 +0200

    added ram

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import computer_parts
 import prg
+import ram
 # imports computer parts
 
 a = 0
@@ -12,4 +13,6 @@ s = 0
 alu = computer_parts.ALU(0,0,0,0)
 prgcnt = computer_parts.Counter(0)
 ramcnt = computer_parts.Counter(0)
+ram = computer_parts.RAM(0,0,ram.ram)
+rom = computer_parts.ROM(0,0,prg.prg)
 # defines all the parts
